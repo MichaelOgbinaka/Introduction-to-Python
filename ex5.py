812b760050f4399ce770ed18b3ba8049a9cf267f
Michael Ogbinaka <168342563+Mogbinaka@users.noreply.github.com>
2025-01-31 13:54:49 -0500

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,26 @@
+a = 4
+b = 5
+c = 6
+
+print("a:", a, "b:", b, "c:", c)
+print()
+print("a + b + c =", a + b + c)
+print("a + b - c =", a + b - c)
+print("b - c - a =", b - c - a)
+print()
+print("c / a =", c / a)
+print("c // a =", c // a)
+print("c % a =", c % a)
+print()
+print("a ** 2=", a ** 2)
+print("a ** 3=", a ** 3)
+print("a ** 4=", a ** 4)
+print()
+print("a + b * c =", a + b * c)
+print("(a + b) * c =", (a + b) * c)
+print("a + b / c =", a + b / c)
+print("a + b // c =", a + b // c)
+print("a + b % c =", a + b % c)
+print("(a + b) / c =", (a + b) / c)
+print("(a + b) // c =", (a + b) // c)
+print("(a + b) % c =", (a + b) % c)
