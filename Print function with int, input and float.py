fceddff07faaf2d074a6168b26ed3d53220b94dc
Michael Ogbinaka <168342563+Mogbinaka@users.noreply.github.com>
2025-01-31 14:03:22 -0500

    Update Print function with int, input and float.py

--- Print function with int, input and float.py
+++ Print function with int, input and float.py
@@ -1,22 +1,35 @@
+# using the print function to assign an integer value to the variable score_1
 print("Assigning an integer value to score_1")
 score_1 = 85
 print("score_1", score_1)
 print("score_1 is", type(score_1))
 print()
+
+# using the print function to determine the class of the value in the variable score_1 = integer
 print("Assigning a decimal value to score_2")
 score_2 = 85.0
 print("score_2", score_2)
 print("score_2 is", type(score_2))
 print()
+
+# using the print function to determine the class of the value in the variable score_1 = decimal
 print("Assigning a string value to score_3")
 score_3 = "85"
 print("score_3", score_3)
 print("score_3 is", type(score_3))
 print()
+
+# using the int function to integate the value given to "please enter an integer:
 number_1 = int(input("Please enter an integer: "))
 print("The value of number_1 is", number_1)
 print("number_1 is", type(number_1))
 print()
+
+# what ever value the user gives, it turns it into an integer because of the int function
+
 number_2 = float(input("Please enter a decimal number: "))
 print("The value of number_2 is", number_2)
 print("number_2 is", type(number_2))
+
+# using the float function to change the value given to "please enter an decimal:" to a decimal
+# what ever value the user gives, it turns it into a decimal because of the float function
