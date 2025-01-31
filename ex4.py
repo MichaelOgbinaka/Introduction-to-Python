d0ffeb53c728d1def68983c0b17bfebf66510a42
Michael Ogbinaka <168342563+Mogbinaka@users.noreply.github.com>
2025-01-31 13:46:36 -0500

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,22 @@
+print("Assigning an integer value to score_1")
+score_1 = 85
+print("score_1", score_1)
+print("score_1 is", type(score_1))
+print()
+print("Assigning a decimal value to score_2")
+score_2 = 85.0
+print("score_2", score_2)
+print("score_2 is", type(score_2))
+print()
+print("Assigning a string value to score_3")
+score_3 = "85"
+print("score_3", score_3)
+print("score_3 is", type(score_3))
+print()
+number_1 = int(input("Please enter an integer: "))
+print("The value of number_1 is", number_1)
+print("number_1 is", type(number_1))
+print()
+number_2 = float(input("Please enter a decimal number: "))
+print("The value of number_2 is", number_2)
+print("number_2 is", type(number_2))
