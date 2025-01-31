6b61817bf646980dc5ee449c2e1f49964eb2b361
Michael Ogbinaka <168342563+Mogbinaka@users.noreply.github.com>
2025-01-31 13:44:39 -0500

    Print Function / Variables.py

--- ex3.py
+++ ex3.py
@@ -1,13 +1,19 @@
+#Second Excercise on Python. Using the print function on Variables
+
+# Created variables
 a = 5
 b = 6
 c = 10
 
+# Using the print function to solve arithmetic problems
 print("a + b + c = ", a + b + c)
 print("a * b + c = ", a * b + c)
 print("a * b * c = ", a * b * c)
 print("c - b - a = ", c - b - a)
 print("a * b / c = ", a * b / c)
 print()
+
+# Using the print function to print out different arguments
 
 print("University of Massachusetts at Boston")
 print("100 Morrissey Boulevard")
