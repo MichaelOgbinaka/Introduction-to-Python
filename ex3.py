eac6ab85465f040270d123311df88d2ec548441e
Michael Ogbinaka <168342563+Mogbinaka@users.noreply.github.com>
2025-01-31 13:42:09 -0500

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,14 @@
+a = 5
+b = 6
+c = 10
+
+print("a + b + c = ", a + b + c)
+print("a * b + c = ", a * b + c)
+print("a * b * c = ", a * b * c)
+print("c - b - a = ", c - b - a)
+print("a * b / c = ", a * b / c)
+print()
+
+print("University of Massachusetts at Boston")
+print("100 Morrissey Boulevard")
+print("02125-3393")
