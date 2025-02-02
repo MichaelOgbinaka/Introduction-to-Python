b11fc79b0bbd81c3c47ec5d475175cf311ac023b
Michael Ogbinaka <michaelogbinaka@gmail.com>
2025-02-02 18:53:10 -0500

    Add files via upload

---
+++ Hw2.py
@@ -0,0 +1,13 @@
+# I request for the users name using the input function and store it in a variable
+
+name = input("what is your name:")
+email = input("what is your email address:")
+team = input("what is your favorite team:")
+major = input("what is your major:")
+
+# I use the print function to print out the variables
+
+print("Name:" , name )
+print("Email:" , email )
+print("Team:" , team )
+print("Major:" , major )
