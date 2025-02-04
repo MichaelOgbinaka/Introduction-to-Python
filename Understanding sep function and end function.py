5df08bd140794ffc30cedd82116f98fc0bec5e95
Michael Ogbinaka <michaelogbinaka@gmail.com>
2025-02-04 14:02:40 -0500

    Understanding sep function and end function.py

---
+++ Understanding sep function and end function.py
@@ -0,0 +1,21 @@
+# Here I created a variable for number 1 and 2
+number_1 = 4
+number_2 = 5
+
+# Here I use the tab function "\t" to create a tab before the variable
+print("number_1:\t", number_1)
+print("number_2:\t", number_2)
+
+print()
+
+# Here I use the end function to let python know that the ends of each line should be a space instead of a return
+print("Line 1", end=" ")
+print("Line 2", end=" ")
+print("Line 3")
+
+# Here I use the sep function to let python know that each argument should be seperated by a comma and space
+print(1, 2, 3, 4, 5, sep=", ")
+
+# Here I use the \\ to let python know that it should print a one of the \
+print('The path is C:\\temp\\data.')
+print("He said \"It\'s me!\"")
