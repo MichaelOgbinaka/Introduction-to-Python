420a4739c1db8b7e08cbbcec21de4da69bd72df2
Michael Ogbinaka <michaelogbinaka@gmail.com>
2025-02-17 14:58:15 -0500

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,16 @@
+print("4 < 8 and 8 < 80:", 4 < 8 and 8 < 80)
+print("4 > 8 or 8 < 80:", 4 > 8 or 8 < 80)
+print("not True:", not True)
+print("not False:", not False)
+
+print()
+
+num_1 = 4
+num_2 = 5
+num_3 = 6
+num_4 = 7
+
+print('num_1 < num_2 and num_3 <= num_4: ', num_1 < num_2 and num_3 <= num_4)
+print('num_1 < num_2 or num_3 > num_4: ', num_1 < num_2 or num_3 > num_4)
+print('num_1 == num_2 and num_3 == num_4: ', num_1 == num_2 and num_3 != num_4)
+print('num_2 == num_2 or num_3 != num_3: ', num_2 == num_2 or num_3 != num_3)
