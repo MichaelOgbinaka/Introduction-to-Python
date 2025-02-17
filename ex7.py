c992961823992ffdf77395b27cc84c982d30bc6f
Michael Ogbinaka <michaelogbinaka@gmail.com>
2025-02-17 14:53:58 -0500

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,38 @@
+number_1 = 4
+number_2 = 5
+
+print("number_1 > number_2: ", number_1 > number_2)
+print("number_1 < number_2: ", number_1 < number_2)
+print("number_1 >= number_2: ", number_1 >= number_2)
+print("number_1 <= number_2: ", number_1 <= number_2)
+print("number_1 == number_2: ", number_1 == number_2)
+print("number_1 != number_2: ", number_1 != number_2)
+
+print()
+
+print("\"abcd\" == \"ABCD\":", "abcd" == "ABCD")
+print("\"abcd\" != \"ABCD\":", "abcd" != "ABCD")
+print("\"abcd\" > \"ABCD\":", "abcd" > "ABCD")
+print("\"abcd\" < \"ABCD\":", "abcd" < "ABCD")
+
+print()
+
+numb_1 = 6
+if numb_1 > 0:
+    print("numb_1 is positive")
+
+numb_1 = 0
+if numb_1 > 0:
+    print("numb_1 is positive")
+else:
+        print("numb_1 is NOT positive")
+
+
+if numb_1 > 0:
+    print("numb_1 is positive")
+elif numb_1 < 0:
+    print("numb_1 is negative")
+else:
+        print("numb_1 is 0")
+
+print()
