abb45fceebced378d713a92381e053b3984362cf
Michael Ogbinaka <michaelogbinaka@gmail.com>
2025-02-17 14:57:45 -0500

    Update and rename ex7.py to If, elif and else function.py

--- If, elif and else function.py
+++ If, elif and else function.py
@@ -1,6 +1,8 @@
+# here I named the variables for 4 and 5
 number_1 = 4
 number_2 = 5
 
+# I use the print function and Boolean expression to print out true or false
 print("number_1 > number_2: ", number_1 > number_2)
 print("number_1 < number_2: ", number_1 < number_2)
 print("number_1 >= number_2: ", number_1 >= number_2)
@@ -17,17 +19,19 @@ print("\"abcd\" < \"ABCD\":", "abcd" < "ABCD")
 
 print()
 
+# I used an If statemnt numb_1 is positive under an initial condition
 numb_1 = 6
 if numb_1 > 0:
     print("numb_1 is positive")
 
+# Using the if and else function
 numb_1 = 0
 if numb_1 > 0:
     print("numb_1 is positive")
 else:
         print("numb_1 is NOT positive")
 
-
+# Using the if, elif and else function
 if numb_1 > 0:
     print("numb_1 is positive")
 elif numb_1 < 0:
